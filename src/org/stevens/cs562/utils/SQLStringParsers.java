2400ea7d4dd13630a054f907a14b631572496da2
faireprogram <michael.zhangwen@yahoo.com>
2015-02-21 20:45:21 -0500

    dd

--- src/org/stevens/cs562/utils/SQLStringParsers.java
+++ src/org/stevens/cs562/utils/SQLStringParsers.java
@@ -12,16 +12,16 @@ public class SQLStringParsers {
 	 */
 	public static boolean parseString() {
 		Pattern p = Pattern.compile("\\s*select\\s+(?<selectcause>.+)\\s+from\\s+(?<fromcause>.+)\\s+where\\s(?<wherecause>.+)\\s*;", Pattern.CASE_INSENSITIVE);
-		Matcher match = p.matcher(s);
-		while(match.find()) {
+		Matcher matchers = p.matcher(s);
+		while(matchers.find()) {
 			System.out.println("I found the text");
-			System.out.println(match.group("selectcause"));
-			System.out.println(match.group("fromcause"));
-			System.out.println(match.group("wherecause"));
-			match.group();
-			match.start();
-			match.end();
-			match.groupCount();
+			System.out.println(matchers.group("selectcause"));
+			System.out.println(matchers.group("fromcause"));
+			System.out.println(matchers.group("wherecause"));
+			matchers.group();
+			matchers.start();
+			matchers.end();
+			matchers.groupCount();
 			System.out.println("hellow world");
 			
 		}
