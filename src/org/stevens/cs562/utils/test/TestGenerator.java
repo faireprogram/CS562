33bf2ee5355f55ed9f70a72a9a02e78a2a60cbe5
faireprogram <michael.zhangwen@yahoo.com>
2015-03-19 15:18:22 -0400

    EMF part

--- src/org/stevens/cs562/utils/test/TestGenerator.java
+++ src/org/stevens/cs562/utils/test/TestGenerator.java
@@ -10,15 +10,31 @@ import org.stevens.cs562.utils.DynamicCompiler;
 public class TestGenerator {
 	
 	public static void main(String[] strings) throws SQLException, IOException {
-		
-		CodeGeneratorComposite composite = new CodeGeneratorComposite(Constants.GENERATE_MF);
+		/*
+		 * EMF
+		 */
+		CodeGeneratorComposite composite = new CodeGeneratorComposite(Constants.GENERATE_EMF);
 		composite.generate();
 		
-//		/*
-//		 * dynamic 
-//		 */
-		DynamicCompiler compiler = new DynamicCompiler(Constants.GENERATE_CODE_MF_MAIN);
+		/*
+		 * dynamic EMF
+		 */
+		DynamicCompiler compiler = new DynamicCompiler(Constants.GENERATE_CODE_EMF_MAIN);
 		compiler.compileAndRun();
+		
+		/*
+		 * MF
+		 */
+//		CodeGeneratorComposite composite = new CodeGeneratorComposite(Constants.GENERATE_MF);
+//		composite.generate();
+//		
+//
+//		
+//		/*
+//		 * dynamic MF
+//		 */
+//		DynamicCompiler compiler = new DynamicCompiler(Constants.GENERATE_CODE_MF_MAIN);
+//		compiler.compileAndRun();
 	}
 	
 
