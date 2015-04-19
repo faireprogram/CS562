28b3c08e3609a5dfd5b0883c1c3f89f6a595a8f1
faireprogram <michael.zhangwen@yahoo.com>
2015-04-19 03:09:44 -0400

    Very file sql

--- verify_sql/Test SQL for EMF SQL 5.sql
+++ verify_sql/Test SQL for EMF SQL 5.sql
@@ -1,21 +1,25 @@
+drop view IF EXISTS h1 CASCADE;
+drop view IF EXISTS h2 CASCADE;
+drop view IF EXISTS h3 CASCADE;
+
 create view  h1(cust, avg_1_quant) as
 select cust, avg(quant)
 from sales
 where state = 'NY' and year = 1997
-group by cust
+group by cust;
 
 create view h2(cust, avg_2_quant) as
 select cust, avg(quant)
 from sales
 where state = 'NJ' and year = 1997
-group by cust
+group by cust;
 
 create view h3(cust, avg_3_quant) as
 select cust, avg(quant)
 from sales
 where state = 'CT' and year = 1997
-group by cust
+group by cust;
 
 select h1.cust, h1.avg_1_quant, h2.avg_2_quant, h3.avg_3_quant
 from h1, h2, h3
-where h1.cust = h2.cust and h2.cust = h3.cust and h1.avg_1_quant > h2.avg_2_quant and h1.avg_1_quant > h3.avg_3_quant
+where h1.cust = h2.cust and h2.cust = h3.cust and h1.avg_1_quant > h2.avg_2_quant and h1.avg_1_quant > h3.avg_3_quant;
--- verify_sql/Test SQL for EMF SQL 6.sql
+++ verify_sql/Test SQL for EMF SQL 6.sql
@@ -1,13 +1,16 @@
+drop view IF EXISTS v1 CASCADE;
+drop view IF EXISTS v2 CASCADE;
+
 create view v1(cust, prod, avg_1_quant) as 
 select cust, prod, avg(quant)
 from sales
-group by cust, prod
+group by cust, prod;
 
 create view v2(prod, avg_2_quant) as 
 select prod, avg(quant)
 from sales
-group by prod
+group by prod;
 
 select v1.cust, v1.prod, v1.avg_1_quant, v2.avg_2_quant
 from v1, v2
-where v1.prod = v2.prod
+where v1.prod = v2.prod;
